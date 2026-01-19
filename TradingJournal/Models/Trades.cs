d6c012d20f7f2c26e0a8d018ed3d015fa0ce2a1b
Pranit Mule <pranitcode18@gmail.com>
2026-01-19 23:55:08 +0530

    Created Model for saving the data.

---
+++ TradingJournal/Models/Trades.cs
@@ -0,0 +1,23 @@
+﻿namespace TradingJournal.Models
+{
+    public class Trades
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string InstrumentType { get; set; } // Equity, F&O
+        public string TradeType { get; set; } // Buy or sell
+        public float EntryPrice { get; set; }
+        public float ExitPrice { get; set; }
+        public int Quantity { get; set; }
+        public float Stoploss { get; set; }
+        public float Target { get; set; }
+        // Risk to Reward
+        public DateTime Entry { get; set; }
+        public DateTime Exit { get; set; }
+        public float Charges { get; set; }
+        public float ProfitAndLoss { get; set; }
+        public string Strategy { get; set; }
+        public string Setup { get; set; }
+        public string Notes { get; set; }
+    }
+}

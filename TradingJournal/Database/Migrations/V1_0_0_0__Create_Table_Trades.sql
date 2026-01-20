77f18477104b032976d920c7dfc0545c132431bb
Pranit Mule <pranitcode18@gmail.com>
2026-01-21 00:01:20 +0530

    Added migration for Create Table Trades

---
+++ TradingJournal/Database/Migrations/V1_0_0_0__Create_Table_Trades.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE "Trades" (
+    "Id" VARCHAR(36) PRIMARY KEY,
+    "Symbol" VARCHAR(50),
+    "InstrumentType" VARCHAR(20),
+    "TradeType" VARCHAR(10),    
+    "EntryPrice" DECIMAL(10,2),
+    "ExitPrice" DECIMAL(10,2),
+    "Quantity" INT,
+    "Stoploss" DECIMAL(10,2),
+    "Target" DECIMAL(10,2),
+    "EntryTime" TIMESTAMP,
+    "ExitTime" TIMESTAMP,
+    "Charges" DECIMAL(10,2),
+    "ProfitAndLoss" DECIMAL(12,2),
+    "Strategy" VARCHAR(100),
+    "Setup" VARCHAR(100),
+    "Notes" TEXT
+);

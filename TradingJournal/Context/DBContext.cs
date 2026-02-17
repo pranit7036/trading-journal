dee8a309e51109c4fd6d30178bc3f58b383f4ecd
Pranit Mule <pranitcode18@gmail.com>
2026-02-17 23:47:21 +0530

    Added Repository and DbContext to save and get data.

---
+++ TradingJournal/Context/DBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradingJournal.Models.Entity;
+namespace TradingJournal.Context
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        public DbSet <TradesEntity> Trades { get; set; }
+    }
+}
---
+++ TradingJournal/Interfaces/Repository/ITradeRepository.cs
@@ -0,0 +1,10 @@
+﻿using TradingJournal.Models.Entity;
+
+namespace TradingJournal.Interfaces.Repository
+{
+    public interface ITradeRepository
+    {
+        public Task<bool> Save(TradesEntity tradesEntity);
+        public Task<List<TradesEntity>> GetData();
+    }
+}

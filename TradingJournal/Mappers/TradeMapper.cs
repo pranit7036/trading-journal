d565fdd5ca386d575d741f9a043b7c4dfae0b28e
Pranit Mule <pranitcode18@gmail.com>
2026-02-18 00:00:05 +0530

    Added Trade Mapper, TradeDto, TradeEntity and standard response.

---
+++ TradingJournal/Mappers/TradeMapper.cs
@@ -0,0 +1,58 @@
+﻿using TradingJournal.Models.Dto;
+using TradingJournal.Models.Entity;
+
+namespace TradingJournal.Mappers
+{
+    public class TradeMapper
+    {
+        public TradeMapper() { }
+        
+        public TradesEntity ConvertDtoToEntity (TradesDto tradeDto, decimal profitAndLoss)
+        {
+            TradesEntity trade = new TradesEntity()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Symbol = tradeDto.Symbol.ToUpper(),
+                InstrumentType = tradeDto.InstrumentType.ToLower(),
+                TradeType = tradeDto.TradeType.ToLower(),
+                EntryPrice = tradeDto.EntryPrice,
+                ExitPrice = tradeDto.ExitPrice,
+                Quantity = tradeDto.Quantity,
+                Stoploss = tradeDto.Stoploss,
+                Target = tradeDto.Target,
+                EntryTime = tradeDto.EntryTime,
+                ExitTime = tradeDto.ExitTime,
+                Charges = tradeDto.Charges,
+                ProfitAndLoss = profitAndLoss,
+                Strategy = tradeDto?.Strategy,
+                Setup = tradeDto?.Setup,
+                Notes = tradeDto?.Notes,
+            };
+            return trade;
+        }
+
+        public TradesDto ConvertEntityToDto (TradesEntity tradeEntity)
+        {
+            TradesDto trade = new TradesDto()
+            {
+                Id = Guid.Parse(tradeEntity.Id),
+                Symbol = tradeEntity.Symbol,
+                InstrumentType = tradeEntity.InstrumentType,
+                TradeType = tradeEntity.TradeType,
+                EntryPrice = tradeEntity.EntryPrice,
+                ExitPrice = tradeEntity.ExitPrice,
+                Quantity = tradeEntity.Quantity,
+                Stoploss = tradeEntity.Stoploss,
+                Target = tradeEntity.Target,
+                EntryTime = tradeEntity.EntryTime,
+                ExitTime = tradeEntity.ExitTime,
+                Charges = tradeEntity.Charges,
+                ProfitAndLoss = tradeEntity.ProfitAndLoss,
+                Strategy = tradeEntity?.Strategy,
+                Setup = tradeEntity?.Setup,
+                Notes = tradeEntity?.Notes
+            };
+            return trade;
+        }
+    }
+}
--- TradingJournal/Models/Entity/TradesEntity.cs
+++ TradingJournal/Models/Entity/TradesEntity.cs
@@ -1,19 +1,22 @@
-﻿namespace TradingJournal.Models
+﻿namespace TradingJournal.Models.Entity
 {
-    public class Trades
+    public class TradesEntity
     {
-        public string Id { get; set; }
-        public string Symbol { get; set; }
-        public string InstrumentType { get; set; } // Equity, F&O
+        public string Id { get; set; } //guid
+        public string Symbol { get; set; } // name of stock i.e. symbol of stock
+        public string InstrumentType { get; set; } // Equity, F&O 
+        // order type MIS or NORMAL
         public string TradeType { get; set; } // Buy or sell
-        public decimal EntryPrice { get; set; }
+        public decimal EntryPrice { get; set; } 
         public decimal ExitPrice { get; set; }
         public int Quantity { get; set; }
         public decimal Stoploss { get; set; }
         public decimal Target { get; set; }
         // Risk to Reward
         public DateTime EntryTime { get; set; }
+        //validate date and time
         public DateTime ExitTime { get; set; }
+        //validate date and time
         public decimal Charges { get; set; }
         public decimal ProfitAndLoss { get; set; }
         public string? Strategy { get; set; }

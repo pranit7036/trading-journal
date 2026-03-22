d5e46a7b2341895d000a79a0c1d8a0cf21b6ca10
Pranit Mule <pranitcode18@gmail.com>
2026-03-22 22:06:37 +0530

    Added User Entity and dto.

---
+++ TradingJournal/Models/Dto/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TradingJournal.Models.Dto
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ TradingJournal/Models/Entity/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace TradingJournal.Models.Entity
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiry { get; set; }
+    }
+}
